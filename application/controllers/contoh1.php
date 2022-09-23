66c158f1151f6eb2266da0cc79b88b2c421a1332
Vilayudha <vilayudha123@gmail.com>
2022-09-23 15:50:27 +0700

    mengubah contoh1

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -3,9 +3,8 @@ class Contoh1 extends CI_Controller
 {
     public function index()
     {
-        echo "<h1>PERKENALKAN</h>";
-        echo "Nama saya VILA YUDHA OEN BASKORO
-            saya tinggal di Nguter Sukoharjo
-            Aku Sayang Kamu";
+        echo "<h1>PERKENALKAN</h><br>";
+        echo "Nama saya VILA YUDHA OEN BASKORO<br>
+            Saya tinggal di Nguter Sukoharjo";
     }
 }
