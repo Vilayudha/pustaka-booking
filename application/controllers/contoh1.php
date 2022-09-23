a1d3b9381779668ba7336063261b2adb229f5634
Vilayudha <Vilayudha123@gmail.com>
2022-09-23 14:09:11 +0700

    menambahkan file

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>PERKENALKAN</h>";
+        echo "Nama saya VILA YUDHA OEN BASKORO
+            saya tinggal di Nguter Sukoharjo
+            Aku Sayang Kamu";
+    }
+}
---
+++ application/controllers/contoh2.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>PERKENALKAN</h>";
+        echo "Nama saya VILA YUDHA OEN BASKORO
+            saya tinggal di Nguter Sukoharjo
+            Sekolah asal SMK N 01 Sukoharjo";
+    }
+}
