41668261cfbe32617e769b919835252536912aa2
Vilayudha <vilayudha123@gmail.com>
2022-10-07 15:13:18 +0700

    stending base url

--- application/views/v_index.php
+++ application/views/v_index.php
@@ -1,33 +1,30 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset=”utf-8”>
-    <title>Web Prog II | Merancang Template sederhana dengan
-codeigniter</title>
-    <link rel=”stylesheet” type=”text/css” href=”<?php echo
-base_url() ?>assets/css/stylebuku.css”>
+    <meta charset="utf-8">
+    <title>Web Prog II | Merancang Template sederhana dengan codeigniter</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/stylebuku.css">
 </head>
 <body>
-    <div id=”wrapper”>
+    <div id="wrapper">
         <header>
             <hgroup>
                 <h1>RentalBuku.net</h1>
-                <h3>Membuat Template Sederhana dengan
-CodeIgniter</h3>
+                <h3>Membuat Template Sederhana dengan CodeIgniter</h3>
             </hgroup>
             <nav>
                 <ul>
-                    <li><a href=”<?php echo
-base_url().’index.php/web’ ?>”>Home</a></li>
-                    <li><a href=”<?php echo
-base_url().’index.php/web/about’ ?>”>About</a></li>
+                    <li><a href="<?php echo
+base_url().'index.php/web' ?>">Home</a></li>
+                    <li><a href="<?php echo
+base_url().'index.php/web/about' ?>">About</a></li>
                 </ul>
             </nav>
-            <div class=”clear”></div>
+            <div class="clear"></div>
         </header>
 <section>
             <h1><?php echo $judul ?></h1>
-            <p align=’justify’>Pada pengertian codeigniter di atas
+            <p align='justify'>Pada pengertian codeigniter di atas
 tadi di jelaskan bahwa codeigniter menggunakan metode MVC. Apa itu
 MVC? Kita juga harus mengetahui apa itu MVC sebelum masuk dan lebih
 jauh dalam belajar codeigniter.</p>
@@ -35,9 +32,9 @@ jauh dalam belajar codeigniter.</p>
 komponen utama menjadi tiga komponen yaitu model, view dan
 controller.</p>
 
-                <ol type=”a”>
+                <ol type="a">
                 <li>Model</li>
-<p align=’justify’>Model adalah kelas yang merepresentasikan atau
+<p align='justify'>Model adalah kelas yang merepresentasikan atau
 memodelkan tipe data yang akan digunakan oleh aplikasi. Model juga
 dapat didefinisakn sebagai bagian penanganan yang berhubungan dengan
 pengolahan atau manipulasi database. Seperti misalnya mengambil data
@@ -46,22 +43,22 @@ intruksi atau fungsi yang berhubung dengan pengolahan database di
 letakkan di dalam model. Sebagai contoh, jika ingin membuat aplikasi
 untuk menghitung luas dan keliling lingkaran, maka dapat memodelkan
 objek lingkaran sebagai kelas model.</p>
-<p align=’justify’>Sebagai catatan, Semua model harus disimpan di
+<p align='justify'>Sebagai catatan, Semua model harus disimpan di
 dalam folder application\models</p>
 <li>View</li>
-<p align=’justify’>View merupakan bagian yang menangani halaman user
+<p align='justify'>View merupakan bagian yang menangani halaman user
 interface atau halaman yang muncul pada user(pada browser). Tampilan
 dari user interface di kumpulkan pada view untuk memisahkannya
 dengan controller dan model sehingga memudahkan web designer dalam
 melakukan pengembangan tampilan halaman website.</p>
 <li>Controller</li>
-<p align=’justify’>Controller merupakan kumpulan intruksi aksi yang
+<p align='justify'>Controller merupakan kumpulan intruksi aksi yang
 menghubungkan model dan view, jadi user tidak akan berhubungan
 dengan model secara langsung, intinya data yang tersimpan di
 database (model) di ambil oleh controller dan kemudian controller
 pula yang menampilkan nya ke view. Jadi controller lah yang mengolah
 intruksi.</p>
-<p align=’justify’>Dari penjelasan tentang model view dan controller
+<p align='justify'>Dari penjelasan tentang model view dan controller
 di atas dapat di simpulkan bahwa controller sebagai penghubung view
 dan model. Misalnya pada aplikasi yang menampilkan data dengan
 menggunakan metode konsep mvc, controller memanggil intruksi pada
@@ -76,7 +73,7 @@ menjadi mudah dan pengembangan aplikasi dapat di lakukan dengan
 cepat dan terstruktur.</p>
 </section>
         <footer>
-            <a href=”http://www.RentalBuku.com”>RentalBuku</a>
+            <a href="http://www.RentalBuku.com">RentalBuku</a>
         </footer>
     </div>
 </body>
