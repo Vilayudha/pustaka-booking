c74e6e7b24277bdb0db46928cf3de7b08601bce6
Vilayudha <vilayudha123@gmail.com>
2022-10-07 15:49:16 +0700

    menambah about

--- application/views/v_index.php
+++ application/views/v_index.php
@@ -1,27 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>Web Prog II | Merancang Template sederhana dengan codeigniter</title>
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/stylebuku.css">
-</head>
-<body>
-    <div id="wrapper">
-        <header>
-            <hgroup>
-                <h1>RentalBuku.net</h1>
-                <h3>Membuat Template Sederhana dengan CodeIgniter</h3>
-            </hgroup>
-            <nav>
-                <ul>
-                    <li><a href="<?php echo
-base_url().'index.php/web' ?>">Home</a></li>
-                    <li><a href="<?php echo
-base_url().'index.php/web/about' ?>">About</a></li>
-                </ul>
-            </nav>
-            <div class="clear"></div>
-        </header>
 <section>
             <h1><?php echo $judul ?></h1>
             <p align='justify'>Pada pengertian codeigniter di atas
@@ -71,10 +47,27 @@ mendesign tampilann aplikasi, karena back-end developer yang
 menangani bagian controller dan modelnya. Jadi pembagian tugas pun
 menjadi mudah dan pengembangan aplikasi dapat di lakukan dengan
 cepat dan terstruktur.</p>
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Imam</li>
+        <li>Nama Belakang : Nawawi</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Ciledug Raya No. 168 Pesanggrahan</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Cirebon</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Catur</li>
+    </ul>
 </section>
-        <footer>
-            <a href="http://www.RentalBuku.com">RentalBuku</a>
-        </footer>
-    </div>
-</body>
-</html>
